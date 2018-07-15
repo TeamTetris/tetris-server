15a65ad692f313737691bbfc72f08336fc075c6d
Maximilian Söchting <maximilian.soechting@student.hpi.de>
2018-07-15 12:12:47 +0200

    Add matchmaking functionality.

--- server.js
+++ server.js
@@ -7,9 +7,26 @@ var io = require('socket.io').listen(server);
 app.get('/', (req, res) => {
   res.send("Server is running");
 });
+/** runningMatches DATA STRUCTURE
 
-
+  runningMatches[matchId] = {
+    players: [
+      {
+        displayName: 'coolboy68',
+        userId: 'PZDoMHjiu8PYfRiKAAAF',
+        points: 5000,
+        placement: 13,
+        connectionStatus: ['connected', 'disconnected'],
+        scoreboardStatus: ['regular', 'endangered', 'spotlighted'],
+        playStatus: ['playing', 'finished']
+      }
+    ],
+    startTime: Date,    
+    nextElimination: Date,
+  };
+ */
 const runningMatches = {};
+let newMatchId = 1000;
 const MATCH_ROOM_PREFIX = "match-";
 const MATCHMAKING_ROOM = "matchmaking";
 
@@ -39,6 +56,7 @@ const getMatchRoomName = (matchId) => {
 
 const addPlayerToMatchmaking = (socket) => {
   socket.join(MATCHMAKING_ROOM);
+  runMatchmaking(socket.server);
 }
 
 const removePlayerFromMatchmaking = (socket) => {
@@ -47,15 +65,44 @@ const removePlayerFromMatchmaking = (socket) => {
 
 const addPlayerToMatch = (socket, matchId) => {
   socket.join(getMatchRoomName(matchId), () => {
-    // TODO: send match info
+    socket.server.to(socket.id).emit('matchInfo', runningMatches[matchId]);
   });
 }
 
 const removePlayerFromMatch = (socket) => {
   for (match of getJoinedMatches(socket)) {
     socket.leave(getMatchRoomName(match));
+    
     // TODO: mark player as left, update match info
   }
+}
+
+const runMatchmaking = (server) => {
+  console.log('Running matchmaking.');
+  const clientsInMatchmaking = server.in(MATCHMAKING_ROOM).clients;
+  server.to(MATCHMAKING_ROOM).emit('matchmakingUpdate', { 'playersInQueue': clientsInMatchmaking.length });
+  if (clientsInMatchmaking.length > 2) {
+    console.log('Starting a game.');
+    const matchInfo = createMatch();
+    server.to(MATCHMAKING_ROOM).emit('matchReady', matchInfo);
+  } else {
+    console.log('Not enough players to start a game.');
+  }
+}
+
+const createMatch = () => {
+  const matchInfo = { 
+    id: newMatchId++, 
+    startTime: new Date(Date.now() + 1000 * 60), // start match in 1 minute
+    joinUntil: new Date(Date.now() + 1000 * 45), // join within 45 seconds
+    nextElimination: new Date(Date.now() + 1000 * 120) // first elimination after 1 minute 
+  };
+  runningMatches[matchInfo.id] = {
+    players: [],
+    startTime: matchInfo.startTime,    
+    nextElimination: matchInfo.nextElimination,
+  };
+  return matchInfo;
 }
 
 server.listen(process.env.PORT || 8081, function() { 
@@ -63,10 +110,10 @@ server.listen(process.env.PORT || 8081, function() {
   io.on('connection', function(socket) {
     socket.on('disconnect', function() {
       if (isInMatchmakingQueue(socket)) {
-        removePlayerFromMatchmaking(socket.id);
+        removePlayerFromMatchmaking(socket.id); // REDUNDANT, socket.io already has sockets leave all rooms on disconnect
       }
       if (isInMatch(socket)) {
-        removePlayerFromMatch(socket);
+        removePlayerFromMatch(socket); // REDUNDANT, socket.io already has sockets leave all rooms on disconnect
       }
     });
 
